9a71ae3b027fd2be2b69c218131669e0f0241b73
khushboo300706-png <khushboo300706@gmail.com>
2026-07-20 00:05:39 +0530

    UI project done

---
+++ 02-components/src/components/Card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Card = () => {
+  return (
+     <div className='Card'>
+    <h1>khushboo</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, cumque?</p>
+    </div>
+  )
+}
+
+export default Card
